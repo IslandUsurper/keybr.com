ce7ce6cc4462f69333908040d7b3f5b62f4775ec
Lyle Mantooth <lyle@menteeth.us>
2025-06-11 13:41:08 -0400

    Flake builds.

--- flake.nix
+++ flake.nix
@@ -26,13 +26,39 @@
           buildNpmPackage {
             pname = "keybr";
             version = "0.0.0";
-            src = ./.;
+            src = fetchFromGitHub {
+              owner = "IslandUsurper";
+              repo = "keybr.com";
+              rev = "46e347ac62db8cd06a80b54d8fdc01ac02c409f4";
+              hash = "sha256-R9mlBa5Iow+WoATNi7mRJAnOsf5x/dH5FZSWtk5XyzA=";
+              leaveDotGit = true;
+            };
+
+            nativeBuildInputs = [ git sqlite ];
 
             npmDeps = importNpmLock {
               npmRoot = ./.;
             };
 
+            npmBuildScript = "build";
+
             npmConfigHook = importNpmLock.npmConfigHook;
+
+            #npmWorkspace = "root";
+
+            postInstall = ''
+              out_keybr=$out/lib/node_modules/keybr.com
+
+              cp -r root/lib $out_keybr/root/lib
+              cp -r root/public/assets $out_keybr/root/public/assets
+
+              mkdir $out/bin
+              cat <<EOF > $out/bin/keybr
+              #!${runtimeShell}
+              exec env NODE_ENV=production ${nodejs}/bin/node --enable-source-maps $out_keybr/root/index.js
+              EOF
+              chmod +x $out/bin/keybr
+            '';
           };
 
         nixosModules.default =
@@ -84,6 +110,31 @@
                 DATABASE_FILENAME = "/var/lib/keybr/database.sqlite";
               };
               environment.etc."keybr/env".source = fmt.generate "keybr-env" cfg.settings;
+              users.users.keybr = {
+                description = "keybr user";
+                group = "keybr";
+                isSystemUser = true;
+
+                # NPM insists on creating ~/.npm
+                home = "/var/cache/keybr";
+              };
+              users.groups.keybr = { };
+              systemd.services.keybr = {
+                description = pkgs.keybr.meta.description;
+                wantedBy = [ "multi-user.target" ];
+                after = [ "network.target" ];
+                serviceConfig = {
+                  ExecStart = "${pkgs.keybr}/bin/keybr";
+                  User = config.users.users.keybr.name;
+                  Group = config.users.users.keybr.group;
+                  CacheDirectory = "keybr";
+                  RuntimeDirectory = "keybr";
+                  StateDirectory = "keybr";
+                };
+                environment = {
+                  NODE_ENV = "production";
+                };
+              };
             };
           };
       }
